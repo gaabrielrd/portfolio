4f6c162b7699e203c70ed95dcecf85c2a715b682
Gabriel Roda <gabriel.roda@uniasselvi.com.br>
2023-09-29 21:25:06 -0300

    added footer + adjustments

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
 export default function Footer() {
-	return <>links aqui</>;
+	return (
+		<div className={styles.footer}>
+			<div className={styles.wrapper}>
+				<p>
+					© 2023 <a href="mailto:gabriel@roda.dev">gabriel@roda.dev</a>
+				</p>
+
+				<p>
+					ícones e imagens por{" "}
+					<Link href={"https://icons8.com"} target="_blank">
+						Icons8
+					</Link>
+				</p>
+				<div className={styles.links}>
+					<Link target="_blank" href={`https://github.com/gaabrielrd`}>
+						Github
+					</Link>
+					<Link
+						target="_blank"
+						href={`https://www.linkedin.com/in/gabriel-roda/`}
+					>
+						LinkedIn
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
